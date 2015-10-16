35db86c9c7a05a00cfa7ce82b93dd6fb5a612fcc
Karolis Astrauka <astrauka@gmail.com>
2015-10-16 18:17:31 +0300

    Add inputs image

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,16 +1,5 @@
 module FormHelper
-  def text_input(f, name)
-    input(f, name, type: :text_field)
-  end
-
-  def input(f, name, type: :text_field)
-    field_set_tag(nil, class: 'form__fieldset') do
-      content_tag(:div, class: 'field') do
-        [
-          f.label(name, class: 'field__label'),
-          f.send(type, name, class: 'field__control'),
-        ].join.html_safe
-      end
-    end
+  def input(attributes)
+    render("shared/forms/#{attributes[:version]}/#{attributes[:type]}", attributes: attributes)
   end
 end
